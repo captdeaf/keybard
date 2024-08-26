6b6845ddca787340905385503003d9d6983356ac
Greg Millam <captdeaf@gmail.com>
2024-08-25 22:58:44 -0700

    kbinfo populated, key map showing as per layout

--- pages/js/main.js
+++ pages/js/main.js
@@ -9,14 +9,42 @@
 function startJSVial() {
   get('#launch').onclick = () => {
     console.log("hi?");
-    Vial.open();
+    doStuff();
   }
 }
 
-function decompress(buffer) {
-  buff = buffer;
-  const blob = new Blob([buffer]);
-  const xrs = new xzwasm.XzReadableStream(blob.stream());
-  const resp = new Response(xrs);
-  return resp.text();
+function getStyleFor(opts) {
+  return {
+    top: Math.floor((opts.y * 100) + 100) + 'px',
+    left: Math.floor((opts.x * 100) + 100) + 'px',
+    width: Math.floor(opts.w * 80) + 'px',
+    height: Math.floor(opts.h * 80) + 'px',
+    position: 'fixed',
+  };
+}
+
+async function doStuff() {
+  await Vial.open();
+  const children = [];
+  for (const [kmid, opts] of Object.entries(Vial.kbinfo.keylayout)) {
+    let text = '"' + opts.row + ',' + opts.col + '"';
+    if (opts.text && opts.text.qmkid) text = opts.text.qmkid;
+    const keyimage = EL('div', {
+      id: kmid,
+      class: 'key',
+      style: getStyleFor(opts),
+    }, EL('p', {}, text));
+    opts.keyimage = keyimage;
+    children.push(keyimage);
+  }
+  const sheet = EL('div', {
+    id: 'kb',
+    style: {
+      position: 'relative',
+      height: '100%',
+      width: '100%',
+    }
+  }, ...children);
+  get('#sheet').innerHTML = '';
+  appendChildren(get('#sheet'), sheet);
 }
--- pages/js/usbvial.js
+++ pages/js/usbvial.js
@@ -6,7 +6,7 @@
 //
 ////////////////////////////////////
 
-const RAW = {
+const RAW = lockValue({
   CMD_VIA_GET_PROTOCOL_VERSION: 0x01,
   CMD_VIA_GET_KEYBOARD_VALUE: 0x02,
   CMD_VIA_SET_KEYBOARD_VALUE: 0x03,
@@ -60,27 +60,47 @@ const RAW = {
   DYNAMIC_VIAL_COMBO_SET: 0x04,
   DYNAMIC_VIAL_KEY_OVERRIDE_GET: 0x05,
   DYNAMIC_VIAL_KEY_OVERRIDE_SET: 0x06,
-};
+});
 
 const MSG_LEN = 32;
 
-function LEFrom(num, bytes) {
+function EndianFrom(num, bytes, little) {
   const ab = new ArrayBuffer(bytes);
   const dv = new DataView(ab);
 
   switch (bytes) {
-    case 2: dv.setInt16(0, num, true); break;
-    case 4: dv.setInt32(0, num, true); break;
+    case 2: dv.setInt16(0, num, little); break;
+    case 4: dv.setInt32(0, num, little); break;
   }
   return Array.from(new Uint8Array(ab));
 }
 
 function LE32(num) {
-  return LEFrom(num, 4);
+  return EndianFrom(num, 4, true);
 }
 
 function LE16(num) {
-  return LEFrom(num, 2);
+  return EndianFrom(num, 2, true);
+}
+
+function BE32(num) {
+  return EndianFrom(num, 4, false);
+}
+
+function BE16(num) {
+  return EndianFrom(num, 2, false);
+}
+
+function convEndian(ary, size) {
+  if (size === 2) {
+    return ary.map((num) => (((num >> 8) & 0xFF) | ((num << 8) & 0xFF00)));
+  } else {
+    return ary.map((num) => (
+      ((num << 24) & 0xFF000000) |
+      ((num << 8) & 0xFF0000) |
+      ((num >> 8) & 0xFF00) |
+      ((num >> 24) & 0xFF)));
+  }
 }
 
 const Vial = {
@@ -98,7 +118,8 @@ const Vial = {
     if (devices.length !== 1) return false;
 
     Vial.device = devices[0];
-    Vial.initDevice().then(() => {});
+    await Vial.device.open()
+    await Vial.initDevice();
 
     return true;
   },
@@ -120,21 +141,33 @@ const Vial = {
     // Callback for when we get a response.
     const cbpromise = new Promise((res, rej) => {
       Vial.listener = (data, ev) => {
-        // console.log(data);
+        console.log(data);
         let ret;
         if (flags.uint16) {
           ret = new Uint16Array(data);
+          if (flags.bigendian) {
+            ret = convEndian(ret, 2);
+          }
         } else if (flags.int16) {
           ret = new Int16Array(data);
+          if (flags.bigendian) {
+            ret = convEndian(ret, 2);
+          }
         } else if (flags.uint32) {
           ret = new Int32Array(data);
+          if (flags.bigendian) {
+            ret = convEndian(ret, 4);
+          }
         } else if (flags.int8) {
           ret = new Int8Array(data);
+          if (flags.bigendian) {
+            ret = convEndian(ret, 4);
+          }
         } else {
           ret = new Uint8Array(data);
         }
-        if (flags.first) {
-          ret = ret[0];
+        if (flags.index != undefined) {
+          ret = ret[flags.index];
         } else if (flags.slice) {
           if (flags.slice.length) {
             ret = ret.slice(...flags.slice);
@@ -158,28 +191,80 @@ const Vial = {
   listener: (data, ev) => {},
 
   initDevice: async function() {
-    await Vial.device.open()
     Vial.device.addEventListener('inputreport', (ev) => {
       // console.log(ev.data);
       if (Vial.listener) {
         x = ev.data;
         Vial.listener(ev.data.buffer, ev);
       }
-    });
-    const kbinfo = await Vial.getKeyboardInfo();
+    })
+    const kbinfo = {};
+    await Vial.getKeyboardInfo(kbinfo);
+    await Vial.getKeyMap(kbinfo);
+    await Vial.getKeyLayout(kbinfo);
     console.log("kbinfo", kbinfo);
+    Vial.kbinfo = kbinfo;
+    return kbinfo;
   },
 
-  getKeyboardInfo: async () => {
-    let ret;
-    const kbinfo = {};
+  getKeyMap: async (kbinfo) => {
+    kbinfo.layers = await Vial.send(RAW.CMD_VIA_GET_LAYER_COUNT, [], {uint8: true, index: 1});
+    const size = kbinfo.layers * kbinfo.rows * kbinfo.cols;
 
-    kbinfo.via_proto = await Vial.send(RAW.CMD_VIA_GET_PROTOCOL_VERSION, [], {int32: true, first: true});
+    const allMaps = [];
+   
+    // All KB Maps are fetchable as one big buffer ... in 28-byte chunks.
+    let offset = 0;
+    const chunksize = 28;
+    const alldata = [];
+    const allkeys = {};
+
+    while (offset < size) {
+      let sz = chunksize;
+      if (sz > size - offset) { sz = size - offset; }
+
+      console.log("getkb", offset, sz);
+
+      // Why in the world does everything else use little endian,
+      // but keymap uses big endian?!
+      const data = await Vial.send(RAW.CMD_VIA_KEYMAP_GET_BUFFER, [...BE16(offset), sz],
+                             {uint16: true, slice: 2, bigendian: true});
+      // console.log("chunk", data);
+      alldata.push(...data);
+
+      offset += chunksize;
+    }
+    kbinfo.keymap = [];
+    kbinfo.rowmap = [];
+
+    // For svalboard, Each "row" is a different cluster. 10 rows = 10 clusters.
+    // 8 fingers, 2 thumbs.
+    for (let l = 0; l < kbinfo.layers; l++) {
+      const layer = [];
+      for (let r = 0; r < kbinfo.rows; r++) {
+        for (let c = 0; c < kbinfo.cols; c++) {
+          const offset = (l * kbinfo.rows * kbinfo.cols) + (r * kbinfo.cols) + c;
+          const raw = RAWCODES_MAP[alldata[offset]];
+          layer.push(raw);
+        }
+      }
+      kbinfo.keymap[l] = layer;
+    }
+  },
+
+  getKeyboardInfo: async (kbinfo) => {
+    let ret;
+
+    kbinfo.via_proto = await Vial.send(RAW.CMD_VIA_GET_PROTOCOL_VERSION, [], {int32: true, index: 0});
     kbinfo.kbid = await Vial.sendVia(RAW.CMD_VIAL_GET_KEYBOARD_ID, [], {uint16: true, slice: [0, 3]});
 
     console.log("kbi", kbinfo);
-    
-    const payload_size = await Vial.sendVia(RAW.CMD_VIAL_GET_SIZE, [], {uint32: true, first: true});
+
+    // Keyboard info is via an xz-compressed JSON blob. Fetched 32 bytse
+    // at a time.
+    //
+    // This mostly contains our layout visualizer for the GUI.
+    const payload_size = await Vial.sendVia(RAW.CMD_VIAL_GET_SIZE, [], {uint32: true, index: 0});
     console.log("payload size", payload_size);
 
     let block = 0;
@@ -199,10 +284,92 @@ const Vial = {
       sz = sz - MSG_LEN;
       block += 1;
     }
+    // Decompress and deJSONify
     const up = [...new Int8Array(payload)];
 
-    kbinfo.payload = JSON.parse(await decompress(new Uint8Array(up).buffer));
+    payload = JSON.parse(await decompress(new Uint8Array(up).buffer));
+    kbinfo.payload = payload;
+
+    kbinfo.layout_labels = payload.layouts.labels;
+    kbinfo.rows = payload.matrix.rows;
+    kbinfo.cols = payload.matrix.cols;
 
     return kbinfo
+  },
+
+  getKeyLayout: async (kbinfo) => {
+    // Layout information.
+    // This is a little oddly laid out, to save space:
+    // [obj, str, str, str, obj, ...]
+    const klayout = kbinfo.payload.layouts.keymap;
+
+    const cur = {
+      color: "#cccccc",
+      labels: [],
+      textColor: "",
+      textSize: [],
+      x: 0,
+      y: 0,
+      w: 1,
+      h: 1,
+      x2: 0,
+      y2: 0,
+      width2: 1,
+      height2: 1,
+      rotation_x: 0,
+      rotation_y: 0,
+      rotation_angle: 0,
+      decal: false,
+      ghost: false,
+      stepped: false,
+      nub: false,
+      profile: "",
+      sm: "",
+      sb: "",
+      st: "",
+    };
+
+    const keylayout = {};
+    for (var r = 0; r < klayout.length; r++) {
+      for (var c = 0; c < klayout[r].length; c++) {
+        const item = klayout[r][c]
+        if (typeof(item) !== 'string') {
+          if ('x' in item) {
+            cur.x += item.x;
+          }
+          if ('y' in item) {
+            cur.y += item.y;
+          }
+          if ('w' in item) {
+            cur.w = item.w
+          } else {
+            cur.w = 1;
+          }
+          if ('h' in item) {
+            cur.h = item.h
+          } else {
+            cur.h = 1;
+          }
+          // Object.assign(cur, item);
+        } else {
+          const rc = item.split(',');
+          const kmid = (parseInt(rc[0]) * kbinfo.cols) + parseInt(rc[1]);
+          console.log("foo", [r, c, cur.x, cur.y]);
+          keylayout[kmid] = {
+            row: rc[0],
+            col: rc[1],
+            text: kbinfo.keymap[0][kmid],
+            ...cur
+          };
+          cur.x += cur.w;
+        }
+      }
+      cur.y += cur.h;
+      cur.x = cur.rotation_x;
+    }
+
+    kbinfo.keylayout = keylayout;
+
+    return kbinfo.keylayout;
   },
 }
