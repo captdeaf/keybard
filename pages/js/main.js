2fc49c40ab27c21ba1169536df19511856a07991
Greg Millam <captdeaf@gmail.com>
2024-08-27 16:52:16 -0700

    Add playback/record options as toggles in about

--- pages/js/main.js
+++ pages/js/main.js
@@ -6,13 +6,31 @@
 //
 ///////////////////////////////////
 
-// TODO: Make these toggle-able in the config.
 PLAYBACK = false;
 RECORDING = false;
 
 function startJSVial() {
+  const doPlayback = get('#do-playback');
+  const doRecord = get('#do-record');
+
+  doPlayback.onchange = addToggle('checked', 'do-playback', false,
+        (enabled) => {
+          doPlayback.checked = enabled;
+          PLAYBACK = enabled;
+        }
+      );
+
+  doRecord.onchange = addToggle('checked', 'do-recording', false,
+        (enabled) => {
+          doRecord.checked = enabled;
+          RECORDING = enabled;
+        }
+      );
+
+  console.log("pbr", PLAYBACK, RECORDING);
+
   if (PLAYBACK) {
-    setTimeout(() => doStuff(), 10);
+    setTimeout(() => { doStuff(); }, 100);
   } else {
     get('#launch').onclick = () => {
       doStuff();
@@ -21,7 +39,7 @@ function startJSVial() {
 }
 
 const GUI = {
-  // The main board. This will be updated with 'API'.
+  // The main board. This will be updated with its 'API'.
   board: {},
 };
 
