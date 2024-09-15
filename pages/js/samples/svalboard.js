577b923cd909f3e3b07a54cba811d44bc292914c
Greg Millam <captdeaf@gmail.com>
2024-09-14 19:43:12 -0700

    Add Dvorak as sample layer

--- pages/js/samples/svalboard.js
+++ pages/js/samples/svalboard.js
@@ -10,4 +10,14 @@ SAMPLE_LAYERS.Svalboard = {
     "KC_L", "KC_O", "KC_N", "KC_NO", "KC_SLASH", "KC_BSLASH", "KC_SCOLON",
     "KC_P", "KC_RBRACKET", "KC_NO"
   ],
+  DVORAK: [
+    "KC_LCTRL", "KC_TAB", "KC_LSHIFT", "KC_ENTER", "TO(0)", "KC_CAPSLOCK" , "KC_K",
+    "KC_I", "KC_U", "KC_P", "KC_Y", "KC_NO" , "KC_J", "LSFT(KC_TAB)", "KC_E", "KC_DOT",
+    "KC_GRAVE", "KC_NO" , "KC_Q", "KC_X", "KC_O", "KC_COMMA", "KC_ESCAPE", "KC_NO" ,
+    "KC_SCOLON", "KC_BSLASH", "KC_A", "KC_QUOTE", "KC_DELETE", "KC_NO" , "KC_LALT",
+    "KC_BSPACE", "MO(4)", "KC_SPACE", "TO(2)", "TG(1)" , "KC_M", "KC_F", "KC_H",
+    "KC_G", "KC_D", "KC_NO" , "KC_W", "KC_RBRACKET", "KC_T", "KC_C", "KC_LBRACKET", "KC_NO" ,
+    "KC_V", "KC_LGUI", "KC_N", "KC_R", "KC_B", "KC_NO" , "KC_Z", "KC_MINUS", "KC_S",
+    "KC_L", "KC_SLASH", "KC_NO"
+  ]
 };
