2ea639f0256728663da4a3007dbebce48d59cc0c
Greg Millam <captdeaf@gmail.com>
2024-10-22 15:51:36 -0700

    Remove rgb since it doesn't seem to work

--- pages/js/kbui/layercolors.js
+++ pages/js/kbui/layercolors.js
@@ -23,36 +23,4 @@ addInitializer('connected', () => {
     await USB.send(0xEE, [0x11, layer, h, s, v]);
   };
 
-  LAYER_COLORS.rgb2hsv = (r, g, b) => {
-    // Taken from stack overflow link
-    r /= 255, g /= 255, b /= 255;
-
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, v = max;
-
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if (max == min) {
-      h = 0; // achromatic
-    } else {
-      switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-
-      h /= 6;
-    }
-
-    console.log('hsv', h, s, v);
-
-    return [ Math.floor(h * 255), Math.floor(s * 255), Math.floor(v * 255) ];
-  }
-
-  LAYER_COLORS.setLayerColorRGB = async (layer, r, g, b) => {
-    const [h, s, v] = LAYER_COLORS.rgb2hsv(r, g, b);
-    console.log([0x11, layer, h, s, v]);
-    await USB.send(0xEE, [0x11, layer, h, s, v]);
-  };
 });
