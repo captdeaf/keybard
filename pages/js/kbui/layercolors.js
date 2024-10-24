132c64a56eb70247aa8873c3a6dca84d5cb4762c
Greg Millam <captdeaf@gmail.com>
2024-10-23 20:40:30 -0700

    Color picker proof of concept

--- pages/js/kbui/layercolors.js
+++ pages/js/kbui/layercolors.js
@@ -12,7 +12,30 @@ const LAYER_COLORS = {
 }
 
 addInitializer('connected', () => {
+  const pickerFloat = get('#colorpicker');
   let layer = 0;
+
+  // From google ai overview
+  function hslToHsv(h, s, l) {
+    s /= 100;
+    l /= 100;
+
+    const v = l + s * Math.min(l, 1 - l);
+    const newS = v === 0 ? 0 : 2 * (1 - l / v);
+
+    return [h, newS * 100, v * 100];
+  }
+
+  const led = get('#color-light');
+
+  const slider = get('#color-slider');
+  slider.onchange = () => {
+    led.style['background-color'] = `hsl(${Math.floor(slider.value)}, 100%, 50%)`;
+    const hsv = hslToHsv(Math.floor(slider.value), 100, 50);
+    console.log('lc', hsv);
+    Vial.sval.setLayerColor(KBINFO, layer, Math.floor((hsv[0]/360)*255), 255, 255);
+  }
+
   LAYER_COLORS.setLayerColor = async (l) => {
     layer = l;
     ACTION.showFloat(pickerFloat);
@@ -23,3 +46,7 @@ addInitializer('connected', () => {
     Vial.sval.setLayerColor(KBINFO, ...args);
   };
 });
+
+// addInitializer('connected', () => {
+  // LAYER_COLORS.setLayerColor(15);
+// }, 10000);
