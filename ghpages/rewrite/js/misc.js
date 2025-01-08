e5b16d2486248b27dae0d2f7b28767572f437578
AutoPages <autopages@keybard.svalboard.app>
2025-01-08 00:25:04 +0000

    Update from rewrite

--- ghpages/rewrite/js/misc.js
+++ ghpages/rewrite/js/misc.js
@@ -37,13 +37,27 @@ function makeEditableName(editable, type, index) {
 
     let name = getEditableName(type, index, "" + index);
 
-    editable.innerText = name;
     editable.setAttribute("title", type + " " + name + " (r-click to change name)");
 
-    editable.oncontextmenu = (ev) => {
-        if (SETTINGS.disableRightClicks) {
-            return true;
-        }
+    const editableContent = EL('div', {}, '' + name);
+
+    editable.style['position'] = 'relative';
+
+    const editIcon = EL(
+      "div",
+      { 
+        class: "edit-icon",
+        style: {
+            float: 'right',
+            position: 'absolute',
+            top: '0px',
+            right: '0px',
+        },
+      },
+      '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg>'
+    );
+    appendChildren(editable, editableContent, editIcon);
+    editIcon.onclick = (ev) => {
         const local = getSaved("names", {});
         ev.preventDefault();
         const newname = prompt("New name for " + type + " " + name);
@@ -65,7 +79,7 @@ function makeEditableName(editable, type, index) {
         }
         setSaved("names", local);
         name = getEditableName(type, index);
-        editable.innerText = name;
+        editableContent.innerText = name;
         editable.setAttribute("title", type + " " + name + " (r-click to change name)");
         editable.setAttribute("layerName", name);
         return false;
